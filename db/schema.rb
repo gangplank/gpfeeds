43f89edd18c303303fb8129ea0f7b8760fe92096
David Monaghan <github@allstruck.com>
2013-05-30 19:13:35 -0700

    updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(:version => 20130528190729) do
   end
 
   create_table "tweets", :force => true do |t|
-    t.integer   "tweet_id"
+    t.string   "tweet_id"
     t.datetime "tweet_time"
     t.string   "name"
     t.string   "screen_name"
