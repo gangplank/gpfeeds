e943de71b9938d2b07a5b5b164b1021187bd9207
David Monaghan <github@allstruck.com>
2013-05-30 20:21:50 -0700

    Added limit on tweet_id integer in existing migration...

--- db/migrate/20130531021937_change_tweets_tweetid_datatype_to_integer.rb
+++ db/migrate/20130531021937_change_tweets_tweetid_datatype_to_integer.rb
@@ -1,7 +1,7 @@
 class ChangeTweetsTweetidDatatypeToInteger < ActiveRecord::Migration
 	def up
 		remove_column :tweets, :tweet_id
-		add_column :tweets, :tweet_id, :integer
+		add_column :tweets, :tweet_id, :integer, :limit=>8
 	end
 
 	def down
