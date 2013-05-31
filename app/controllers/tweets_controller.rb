897a142d9766f96053a08672fd354e6c167190c2
jeremyescott <jeremyescott@gmail.com>
2013-05-30 18:26:26 -0700

    notes added to tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,11 @@ class TweetsController < ApplicationController
   respond_to :json
 
   def index
+    #to-do 
+    
+    #to-do write a rescue
     Tweet.pull_tweets
+    
     @tweet = Tweet.find(:all, :order=>"tweet_id DESC", :limit => 5)
     render :json => @tweet.to_json   
   end
