e751c136d3f850dd8e25481ce8c16d600d3684ee
David Monaghan <github@allstruck.com>
2013-06-08 04:10:57 -0700

    Actually fixed date/time issue on recurring events... pimply and slainly not doing dumb stuff. You know who you are.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,18 +3,18 @@ class Event
   require 'json' 
   
   def self.pull_events
-    url = "https://www.googleapis.com/calendar/v3/calendars/gangplankhq.com_0fgcnbe2jug0b1bi43m5qv86s8%40group.calendar.google.com/events?futureevents=true&timeMin=#{today}&timeMax=#{future}&key=#{ENV['GOOGLE_CAL_KEY']}"
+    url = "https://www.googleapis.com/calendar/v3/calendars/gangplankhq.com_0fgcnbe2jug0b1bi43m5qv86s8%40group.calendar.google.com/events?futureevents=true&timeMin=#{today}&timeMax=#{future}&key=#{ENV['GOOGLE_CAL_KEY']}&singleEvents=true&orderBy=startTime"
     JSON.parse(open(url).read)["items"].map do |e|
       # Get the recurring rule string from the event
       rrule = CGI::parse(e["recurrence"].find {|s| s.include?("RRULE")}) if e["recurrence"]
       # Set the correct Date for the event based on the recurring settings.
-      if rrule
-        range = (DateTime.now..DateTime.now + 3).to_a
-        start = DateTime.parse(e["start"]["dateTime"])
-        date = range[range.index { |dt| dt.wday == start.wday }]
-        corrected_start = DateTime.new(date.year, date.month, date.day, start.hour, start.min)
-        e["start"]["dateTime"] = corrected_start
-      end
+      # if rrule
+      #   range = (DateTime.now..DateTime.now + 3).to_a
+      #   start = DateTime.parse(e["start"]["dateTime"])
+      #   date = range[range.index { |dt| dt.wday == start.wday }]
+      #   corrected_start = DateTime.new(date.year, date.month, date.day, start.hour, start.min)
+      #   e["start"]["dateTime"] = corrected_start
+      # end
       e.keep_if { |key, value | ["id", "summary", "status", "organizer", "location", "start"].include?(key) }
     end
   end
@@ -26,7 +26,7 @@ class Event
   end
   
   def self.future
-    format_time(DateTime.now + 3) 
+    format_time(DateTime.now + 7)
   end
 
   #refactor this, specifically UTC offset
